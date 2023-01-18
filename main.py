459a939769e17759984d2312fd9431f9d871b913
ruzicka <tartanec@gmail.com>
2023-01-18 10:27:44 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from twitch import TwitchAPI
+import json
+from time import sleep
+
+def main():
+    # get settings from init json
+    with open('init.json', 'r') as f:
+        conf = json.load(f)
+    api = TwitchAPI(**conf)
+    while True:
+        if not api.isStreamerOnline():
+            sleep(3600)
+            continue
+        for _ in range(12):
+            api.getPoints()
+            sleep(300)
+
+if __name__ == '__main__':
+    main()
